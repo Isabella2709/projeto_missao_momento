6265dd3851f5df7392bb649d6753ab58997e9aaf
Isabella Ribas27 <isabellards@outlook.com>
2024-03-23 20:05:38 -0300

    Paginas responsivas terminadas

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function formenvi() {
+  let nome = document.getElementById("name");
+
+  if (nome != null) {
+    alert("Obrigado por seu contato, responderemos assim que possivel.");
+  }
+}
+
+function compra() {
+  alert(
+    "Compra Realizada com Sucesso, Agradecemos a preferencia, informações de Frete Chegara no Seu email"
+  );
+}
